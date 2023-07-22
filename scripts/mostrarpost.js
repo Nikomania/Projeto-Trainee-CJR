ff1ae61ad50d06b7739eb35d4f8eca3ec038bcfe
arturmald <arturalmeidamaldaner@gmail.com>
2023-07-22 11:42:16 -0300

    git pull

---
+++ scripts/mostrarpost.js
@@ -0,0 +1,12 @@
+const modal = document.getElementById("modaloverlay")
+const btmcomentar = document.querySelector(".buttonComentar")
+btmcomentar.addEventListener("click", ()=>{
+    modal.classList.toggle("visible")
+})
+const btmcancelar = document.getElementById("cancel")
+btmcancelar.addEventListener("click", ()=>{
+    modal.classList.toggle("visible")
+})
+var simplemde = new SimpleMDE({
+    toolbar: ["bold", "italic", "heading", "link", "image", "guide"],
+});
